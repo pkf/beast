e6c2befb0b3cab08df7a8d520c1bfc1bb78b39c6
叶新伟 <yexinwei@yixia.com>
2018-02-19 21:03:25 +0800

    fix

--- test.go
+++ test.go
@@ -6,14 +6,24 @@ import (
 
 func main() {
 	fmt.Println("begin")
-	a := make([]byte, 1024)
-	b := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	a := make([]byte, 100)
+	b := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-	length := 5
-	fmt.Println(b[length:])
+	//length := 5
+	//fmt.Println(b[length:])
 	copy(a, b)
+	//fmt.Println(a)
+
+	//a = append(a, b...)
+	//fmt.Println(a)
+
+	length2 := len(a)
+	a = a[0:length2]
+	copy(a, b)
+	fmt.Println(a, length2)
+
+	a = append(a, b...)
 	fmt.Println(a)
 
 	fmt.Println("end")
-
 }
